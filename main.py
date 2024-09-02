82358e49e4c4e2949df52ae6b3567543df8cda1f
raj <harshithraj41@gmail.com>
2024-09-02 13:08:54 +0530

    name to maary

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('mary')
+    print_hi('maary')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
